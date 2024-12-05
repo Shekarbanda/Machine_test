fa88ca8dbe729cf70ff270434c7e5341e5d3bb36
Banda Shekar <bandashekar8688@gmail.com>
2024-12-06 02:08:31 +0530

    Updated

--- frontend/src/API_URL/api.js
+++ frontend/src/API_URL/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://employee-management-system-backend-nvdf.onrender.com',
+    baseURL: 'https://employee-management-system-backend-nvdf.onrender.com/api',
 });
 
 export default api;
